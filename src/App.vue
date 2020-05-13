936e15f7e5ec7f9ede50c71dcb5eb2284c41150d
Artur <algoz098@gmail.com>
2020-05-13 10:33:20 -0300

    POC

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1 class="q-my-md">
+        Audio-recorder
+      </h1>
+
+      <p>
+        A Lightweight Vue Component for Audio recording
+      </p>
+    </div>
+
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6">Basic Example</div>
+        <div class="text-subtitle2">Without Customization</div>
+      </q-card-section>
+
+      <q-card-section>
+        <audio-recorder
+          v-model="play"
+          @audio-url="url = $event"
+        />
+      </q-card-section>
+    </q-card>
+
+    <audio-recorder
+      v-model="play"
+      @audio-url="url = $event"
+    >
+      <button @click="play = !play">
+        CUSTOM PLAY
+      </button>
+
+      {{ play }}
+
+      <audio
+        v-if="url"
+        controls
+        :src="url"
+      />
+
+      {{ url }}
+    </audio-recorder>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script src="https://cdn.jsdelivr.net/npm/quasar@1.11.1/dist/quasar.umd.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/quasar@1.11.1/dist/lang/pt-br.umd.min.js"></script>
 
+<script>
+import AudioRecorder from './components/AudioRecorder'
+// Quasar.lang.set(Quasar.lang.ptBr)
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    AudioRecorder
+  },
+
+  data () {
+    return {
+      play: false,
+      url: ''
+    }
   }
 }
 </script>
 
 <style>
+ @import "https://cdn.jsdelivr.net/npm/quasar@1.11.1/dist/quasar.min.css";
+ @import "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
 }
+
+h1 {
+  font-size: 50px;
+  line-height: 1;
+}
+
 </style>
---
+++ src/components/AudioRecorder.vue
@@ -0,0 +1,105 @@
+<template>
+  <div
+    v-if="loaded"
+    class="__audio-recorder"
+  >
+    <slot />
+    <button @click="start">
+      Start
+    </button>
+    <button @click="stop">
+      Stop
+    </button>
+
+    <span v-if="recording">
+      {{ timePassed }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AudioRecorder',
+
+  props: {
+    value: {
+      type: Boolean
+    }
+  },
+
+  data () {
+    return {
+      loaded: true,
+      recording: false,
+      mediaRecorder: null,
+      audioChunks: [],
+
+      counter: null,
+      timePassed: '00:00',
+      seconds: 0
+    }
+  },
+
+  watch: {
+    value (aft) {
+      if (aft) this.start()
+      else this.stop()
+    }
+  },
+
+  created () {
+    if (this.value) this.load()
+  },
+
+  methods: {
+    load () {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          this.mediaRecorder = new MediaRecorder(stream)
+          this.audioChunks = []
+
+          this.mediaRecorder.addEventListener("dataavailable", event => {
+            this.audioChunks.push(event.data);
+          })
+
+          this.mediaRecorder.addEventListener("stop", this.handleStop)
+          this.start()
+        })
+    },
+
+    start () {
+      if (!this.mediaRecorder) return this.load()
+      this.recording = true
+      this.mediaRecorder.start()
+      this.$emit('input', true)
+
+      this.counter = setInterval(this.handleInterval, 1000)
+    },
+
+    stop () {
+      this.recording = false
+      this.mediaRecorder.stop()
+      clearInterval(this.counter)
+    },
+
+    handleInterval () {
+      this.seconds++
+      this.timePassed = new Date(this.seconds * 1000).toISOString().substr(14, 5)
+      this.$emit('seconds', this.seconds)
+      this.$emit('time-passed', this.timePassed)
+    },
+
+    handleStop () {
+      const audioBlob = new Blob(this.audioChunks)
+      const audioUrl = URL.createObjectURL(audioBlob)
+
+      this.$emit('input', false)
+      this.$emit('audio-blob', audioBlob)
+      this.$emit('audio-url', audioUrl)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
